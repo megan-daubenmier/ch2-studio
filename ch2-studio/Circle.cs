575766d3ce8fd54da66dfff45681a0f71736f1ea
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-07-14 20:23:14 -0500

    Add project files.

---
+++ ch2-studio/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch2_studio
+{
+    public class Circle
+    {
+        public static double areaOfCircle(double radius)
+        {
+            return Math.Round(Math.PI * Math.Pow(radius, 2), 3);
+        }
+        public static double diameterOfCircle(double radius)
+        {
+            return 2 * radius;
+        }
+        public static double circumferenceOfCircle(double radius)
+        {
+            return Math.Round(2 * Math.PI * radius, 3);
+        }
+    }
+}
---
+++ ch2-studio/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ch2_studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a radius: ");
+            string input = Console.ReadLine();
+            double radius;
+            if (!double.TryParse(input, out radius))
+            {
+                Console.WriteLine("Error: Non-numeric value entered.");
+
+            } else
+            {
+                while (radius < 0)
+                {
+                    Console.WriteLine("Please enter a value greater than 0.");
+                    radius = double.Parse(Console.ReadLine());
+                }
+
+                double area = Circle.areaOfCircle(radius);
+                double diameter = Circle.diameterOfCircle(radius);
+                double circumference = Circle.circumferenceOfCircle(radius);
+
+                Console.WriteLine("The diameter of a circle with radius " + radius + " is: " + diameter);
+                Console.WriteLine("The circumference of a circle with radius " + radius + " is: " + circumference);
+                Console.WriteLine("The area of a circle with radius " + radius + " is: " + area);
+
+                Console.WriteLine("How many miles does your car travel per gallon of gas?");
+                double mpg = double.Parse((Console.ReadLine()));
+
+                double gallonsAroundCircle = circumference / mpg;
+
+                Console.WriteLine("Your car will use " + gallonsAroundCircle + " gallons of gas to travel around a circle with radius " + radius + " miles.");
+            }
+        }
+    }
+}
